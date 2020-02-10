c219d5339f4687954575c35260418d4f7f4b9bb7
pallavi0002 <60617918+pallavi0002@users.noreply.github.com>
2020-02-10 13:03:29 +0530

    java

---
+++ innerclasses/AnonymousInnerClass.java
@@ -0,0 +1,22 @@
+package innerclasses;
+
+public class AnonymousInnerClass {
+	
+	import abstraction.AbstractDemo;
+
+	public class AnonymousInnerClass {
+
+	    public static void main(String[] args) {
+
+	        AbstractDemo demo=new AbstractDemo() {
+	            @Override
+	            public void showData() {
+
+	            }
+	        };
+
+
+	    }
+	}
+}
+	
