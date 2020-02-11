0eb36bee05f5acce419861305e376da416d0dd62
pallavi0002 <60617918+pallavi0002@users.noreply.github.com>
2020-02-11 13:10:18 +0530

    exam

--- exam/ReverseString.java
+++ exam/ReverseString.java
@@ -3,16 +3,16 @@ package exam;
 public class ReverseString {
 	
 	   public static void main(String[] args) {
-	        String str = "Welcome to Beginnersbook";
+	        String str = "i am Pallavi";
 	        String reversed = reverseString(str);
-	        System.out.println("The reversed string is: " + reversed);
+	        System.out.println("i am Pallavi: " + reversed);
 	    }
 
 	    public static String reverseString(String str)
 	    {
 	        if (str.isEmpty())
 	            return str;
-	        //Calling Function Recursively
+	        
 	        return reverseString(str.substring(1)) + str.charAt(0);
 	    }
 	}
