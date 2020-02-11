f7e1b0f919ca97c7a62cef52e94426a374d0eb9e
pallavi0002 <60617918+pallavi0002@users.noreply.github.com>
2020-02-11 11:05:22 +0530

    exam

---
+++ exam/SumOfArray.java
@@ -0,0 +1,17 @@
+package exam;
+
+public class SumOfArray {
+	
+	
+	    public static void main(String[] args) {
+	        int ar[] = {2,3,4,5};
+	        int sum=0;
+	        for(int i=0;i<ar.length;i++){
+	            sum = sum+ar[i];
+	        }
+
+	        System.out.println(sum);
+	    }
+	}
+
+
