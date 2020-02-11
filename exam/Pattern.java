31d0088181434084d95293aca2b0d30f7d36f44f
pallavi0002 <60617918+pallavi0002@users.noreply.github.com>
2020-02-11 13:10:36 +0530

    exam

--- exam/Pattern.java
+++ exam/Pattern.java
@@ -6,6 +6,7 @@ public class Pattern {
 	    public static void main(String[] args) {
 	        for (int i = 0; i < 5; i++) {
 
+	           
 	            for (int j = 5 - i; j > 1; j--) {
 	              
 	                System.out.print(" ");
